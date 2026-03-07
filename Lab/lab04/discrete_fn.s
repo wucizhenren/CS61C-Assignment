df33dbe93e3ce0459e5f9b879712c021e74dfe66
wucizhenren <1713715022@qq.com>
2026-03-07 21:13:50 +0800

    lab04

--- Lab/lab04/discrete_fn.s
+++ Lab/lab04/discrete_fn.s
@@ -77,7 +77,12 @@ main:
 # Think: why might having a1 be useful?
 f:
     # YOUR CODE GOES HERE!
-
+    addi a0,a0,3
+    addi t0,x0,4
+    mul t0,t0,a0
+    add a1,t0,a1
+    lw a0,0(a1)
+    
     jr ra               # Always remember to jr ra after your function!
 
 print_int:
--- Lab/lab04/megalistmanips.s
+++ Lab/lab04/megalistmanips.s
@@ -67,19 +67,32 @@ map:
     # we call. this is to enforce the abstraction barrier of calling convention.
 mapLoop:
     add t1, s0, x0      # load the address of the array of current node into t1
+    lw t1,0(t1)
     lw t2, 4(s0)        # load the size of the node's array into t2
 
-    add t1, t1, t0      # offset the array address by the count
+    addi t3,x0,4
+    mul t3,t3,t0
+    add t1, t1, t3      # offset the array address by the count
     lw a0, 0(t1)        # load the value at that address into a0
-
+#problem 3
+    addi sp,sp,-12
+    sw t0,0(sp)
+    sw t1,4(sp)
+    sw t2,8(sp)
     jalr s1             # call the function on that value.
-
+    lw t0,0(sp)
+    lw t1,4(sp)
+    lw t2,8(sp)
+    addi sp,sp,12
+    
     sw a0, 0(t1)        # store the returned value back into the array
+#problem 2
     addi t0, t0, 1      # increment the count
     bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
 
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+#problem 1
+    lw a0, 8(s0)        # load the address of the next node into a0
+    mv a1,s1        # put the address of the function back into a1 to prepare for the recursion
 
     jal  map            # recurse
 done:
